e7185373b9f2f152c845bc25c5c6462c1c6dfa71
Lukas Köhler <hi@lukkoeh.com>
2024-02-28 12:47:00 +0100

    Add Icons, Add Framework7

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,8 @@
 <script>
-import { RouterLink, RouterView } from 'vue-router'
-import {f7App, f7Navbar, f7View} from 'framework7-vue';
 </script>
 
 <template>
   <f7-app>
-    <f7-view>
-      <h1>A very simple and legit text</h1>
-    </f7-view>
+    <p>Hallo Welt!</p>
   </f7-app>
 </template>
